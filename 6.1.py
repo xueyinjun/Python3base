782260346ebf83b6c4233ab86cab5e54956ed443
xueyinjun <987886748@qq.com>
2019-04-21 03:54:20 +0800

    Create 6.1.py
    -Ajax-

---
+++ 6.1.py
@@ -0,0 +1,69 @@
+#encoding=utf-8
+#base_url表示请求的URL前半部分，接下来构造参数字典，调用urlencode方法将参数转换成需要的参数,用requests请求这个链接加入headers参数，然后判断响应状态码，如果是200调用json方法将内容解析JSON，
+import requests
+from urllib.parse import urlencode
+from pyquery import PyQuery as pq
+from pymongo import MongoClient
+
+base_url = 'https://m.weibo.cn/api/container/getIndex?'
+headers = {
+    'Host':
+    'm.weibo.cn',
+    'Referer':
+    'https://m.weibo.cn/u/2830678474',
+    'User-Agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'X-Requested-With':
+    'XMLHttpRequest',
+}
+client = MongoClient(host='127.0.0.1', port=27017)
+db = client['weibo']
+collection = db['weibo']
+max_page = 10
+
+
+def get_page(page):
+    params = {
+        'type': 'uid',
+        'value': '2830678474',
+        'containerid': '1076032830678474',
+        'page': page
+    }
+    url = base_url + urlencode(params)
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.json(), page
+    except requests.ConnectionError as e:
+        print('Error', e.args)
+
+
+def parse_page(json, page: int):
+    if json:
+        items = json.get('data').get('cards')
+        for index, item in enumerate(items):
+            if page == 1 and index == 1:
+                continue
+            else:
+                item = item.get('mblog', {})
+                weibo = {}
+                weibo['id'] = item.get('id')
+                weibo['text'] = pq(item.get('text')).text()
+                weibo['attitudes'] = item.get('attitudes_count')
+                weibo['comments'] = item.get('comments_count')
+                weibo['reposts'] = item.get('reposts_count')
+                yield weibo
+
+
+def save_to_mongo(result):
+    if collection.insert(result):
+        print('Saved to Mongo')
+
+
+if __name__ == '__main__':
+    for page in range(1, max_page + 1):
+        json = get_page(page)
+        results = parse_page(*json)
+        for result in results:
+            print(result)
+            save_to_mongo(result)
